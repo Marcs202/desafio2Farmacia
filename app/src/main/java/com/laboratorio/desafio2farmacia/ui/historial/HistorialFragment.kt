fb3b4dadf1c9d6149cabf52c185ecafaafb55ea3
marcs202 <marcosjhs554@gmail.com>
2024-04-11 21:47:49 -0600

    Navegacion funcional

---
+++ app/src/main/java/com/laboratorio/desafio2farmacia/ui/historial/HistorialFragment.kt
@@ -0,0 +1,43 @@
+package com.laboratorio.desafio2farmacia.ui.medicamentos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.laboratorio.desafio2farmacia.databinding.FragmentHistorialBinding
+
+
+class HistorialFragment : Fragment() {
+
+    private var _binding: FragmentHistorialBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val medicamentosViewModel =
+            ViewModelProvider(this).get(MedicamentosViewModel::class.java)
+
+        _binding = FragmentHistorialBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHistorial
+        medicamentosViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = "Fragment de historial"
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/laboratorio/desafio2farmacia/ui/medicamentos/MedicamentosViewModel.kt
@@ -0,0 +1,13 @@
+package com.laboratorio.desafio2farmacia.ui.medicamentos
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MedicamentosViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
